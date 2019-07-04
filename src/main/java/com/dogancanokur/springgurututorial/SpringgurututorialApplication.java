a6ffdf29a15be3e97c9941acd41b05cbe47af8c3
dogancanokur <ahmet.okur.94@gmail.com>
2019-07-04 11:44:47 +0300

    start

---
+++ src/main/java/com/dogancanokur/springgurututorial/SpringgurututorialApplication.java
@@ -0,0 +1,13 @@
+package com.dogancanokur.springgurututorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgurututorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgurututorialApplication.class, args);
+    }
+
+}
