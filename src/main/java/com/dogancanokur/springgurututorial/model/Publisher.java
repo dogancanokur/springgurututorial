8704464c546a3bbca3483aec8fb4fb4e2cef7f2d
dogancanokur <ahmet.okur.94@gmail.com>
2019-07-04 15:59:14 +0300

    updated bootstrap class for adding rod added BookController updated Publisher class for equal() hashcode() and toString() created books template

--- src/main/java/com/dogancanokur/springgurututorial/model/Publisher.java
+++ src/main/java/com/dogancanokur/springgurututorial/model/Publisher.java
@@ -11,21 +11,8 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
     private String address;
-
-    public Publisher() {
-    }
-
-    public Publisher(String name) {
-        this.name = name;
-    }
-
-    public Publisher(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
 
     public Long getId() {
         return id;
@@ -49,5 +36,29 @@ public class Publisher {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
